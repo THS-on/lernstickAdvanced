1ce342fbddf2757593585ff1f311c2bf21f626b4
THS-on <mail@thson.de>
2016-10-27 00:39:04 +0200

    Small fixes in constants.ci and image.sh

--- container/rkt/image.sh
+++ container/rkt/image.sh
@@ -6,16 +6,16 @@ sudo debootstrap --include=git,uuid-runtime,debootstrap,libxml-parser-perl,gfxbo
 
 echo "Start image creation"
 #Create custom rkt image 
-sudo acbuild begin rootfs-debian
+sudo acbuild begin ./rootfs-debian
 sudo acbuild set-name lernstick/lernstick
 sudo acbuild label add version "9"
 #Use custom live build version
 sudo acbuild run -- wget http://mirror.thson.de/lernstick/pool/main/l/live-build/live-build_5.0~a5-1%2blernstick2_all.deb
 sudo acbuild run -- dpkg -i live-build_5.0~a5-1+lernstick2_all.deb
 sudo acbuild run -- rm live-build_5.0~a5-1+lernstick2_all.deb
-# Using own reposistory for custom constans and fixed build_dvd.sh
+#Using own reposistory for custom constans and fixed build_dvd.sh
 sudo acbuild set-exec -- /bin/bash -c "git clone https://github.com/THS-on/lernstickAdvanced /lernstickCI && cd /lernstickCI && git checkout debian9-ci && ./build_dvd.sh -c ./container/constants.ci"
-# build is specified in run command
+#build is specified in run command
 sudo acbuild mount add build /build
 #Create lernstick.aci and overite old versions
 sudo acbuild write --overwrite lernstick.aci
